be2c7bd649f6c3dd8410e0558767c85e52418b2e
GabrielRM <gabrielrmodesto5@gmail.com>
2019-01-02 20:28:14 -0200

    Coded query for update, read, delete and select

--- src/todo/query.clj
+++ src/todo/query.clj
@@ -13,3 +13,20 @@
     (insert items
             (values {:title title :description description})))
 
+;;deleting items from db
+(defn delete-todo [id]
+    (delete items
+        (where {:id [=id]})))
+
+;;update items on db
+(defn update-todo [id title is-complete]
+    (update items
+        (set-fields {:title title
+                     :is-complete is-complete})
+        (where {:id [=id]})))
+
+;;select an especific item from db
+(defn get-todo [id]
+    (first
+        (select items
+            (where {:id [=id]}))))
